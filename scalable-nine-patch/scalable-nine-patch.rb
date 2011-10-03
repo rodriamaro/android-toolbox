b70e3d081cfa84374a24a00bb4836a8903ae5d09
Lorenzo Villani <lorenzo@villani.me>
2011-10-03 18:23:43 +0200

    New `scalable-nine-patch` tool.

---
+++ scalable-nine-patch/scalable-nine-patch.rb
@@ -0,0 +1,245 @@
+#!/usr/bin/ruby -w
+#
+# scalable-nine-patch.rb - Create Android NinePatch images from SVGs
+# Copyright (C) 2011  Lorenzo Villani
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Usage:
+# scalable-nine-patch.rb input output x y
+#
+# The input SVG _MUST_ contain four paths with the following IDs:
+# - padding-x
+# - padding-y
+# - stretch-x
+# - stretch-y
+#
+# They can be placed anywhere in the canvas (even outside) and the stroke must
+# be at least one pixel wide.
+
+require "rubygems"
+require "RMagick"
+require "nokogiri"
+
+include Magick
+
+# ------------------------------------------------------------------------------
+# Classes
+# ------------------------------------------------------------------------------
+
+class Path
+  attr_accessor :x, :y, :width, :height
+end
+
+class Svg
+  def initialize(path)
+    @svg = Nokogiri::XML(open(path))
+
+    @path = path
+    @height = @svg.root.attribute("height").value().to_i()
+    @width = @svg.root.attribute("width").value().to_i()
+  end
+
+  def path()
+    return @path
+  end
+
+  def padding_x()
+    return get_path("padding-x")
+  end
+
+  def padding_y()
+    return get_path("padding-y")
+  end
+
+  def stretch_x()
+    return get_path("stretch-x")
+  end
+
+  def stretch_y()
+    return get_path("stretch-y")
+  end
+
+  def height()
+    return @height
+  end
+
+  def width()
+    return @width
+  end
+
+  # ----------------------------------------------------------------------------
+  # "Private" Methods
+  # ----------------------------------------------------------------------------
+
+  def get_path(name)
+    path = @svg.xpath("//svg:svg/svg:path[@id='#{name}']")
+
+    return deconstruct_path_attributes(path.attribute("d").value())
+  end
+
+  def deconstruct_path_attributes(attributes_string)
+    path = Path.new
+
+    if attributes_string =~ /m -?(\d+(\.\d+)?),-?(\d+(\.\d+)?) -?(\d+(\.\d+)?),-?(\d+(\.\d+)?)/ then
+      path.x = $1.to_i()
+      path.y = $3.to_i()
+      path.width = $5.to_i()
+      path.height = $7.to_i()
+    end
+
+    return path
+  end
+end
+
+class ScaledSvg
+  def initialize(svg, width, height)
+    @svg = svg
+    @width = width
+    @height = height
+    @scale_ratio_x = @width.to_f() / svg.width().to_f()
+    @scale_ratio_y = @height.to_f() / svg.height().to_f()
+  end
+
+  def padding_x()
+    return scale(@svg.padding_x())
+  end
+
+  def padding_y()
+    return scale(@svg.padding_y())
+  end
+
+  def stretch_x()
+    return scale(@svg.stretch_x())
+  end
+
+  def stretch_y()
+    return scale(@svg.stretch_y())
+  end
+
+  def scale_ratio_x()
+    return @scale_ratio_x
+  end
+
+  def scale_ratio_y()
+    return @scale_ratio_y
+  end
+
+  def height()
+    return @height
+  end
+
+  def width()
+    return @width
+  end
+
+  # ----------------------------------------------------------------------------
+  # "Private" Methods
+  # ----------------------------------------------------------------------------
+
+  def scale(what)
+    what.x *= @scale_ratio_x
+    what.y *= @scale_ratio_y
+    what.width *= @scale_ratio_x
+    what.height *= @scale_ratio_y
+
+    return what
+  end
+end
+
+# ------------------------------------------------------------------------------
+# Methods
+# ------------------------------------------------------------------------------
+
+def create_scaled_svg(path, width, height)
+  svg = Svg.new(path)
+
+  return ScaledSvg.new(svg, width, height)
+end
+
+def inkscape_export_svg(svg, input, output)
+  `inkscape -z -C -w #{svg.width()} -h #{svg.height()} -e #{output} #{input}`
+end
+
+def rmagick_extend_border(input)
+  pic = ImageList.new(input).at(0)
+  pic.background_color = "None"
+
+  new_width = pic.columns + 2
+  new_height = pic.columns + 2
+
+  pic = pic.extent(new_width, new_height, 1, 1)
+  pic.write(input)
+end
+
+def rmagick_add_guides(scaled_svg, input)
+  pic = ImageList.new(input).at(0)
+  pic.background_color = "None"
+
+  width = pic.columns
+  height = pic.rows
+
+  draw = Draw.new
+  draw.stroke('black')
+  draw.stroke_antialias('false')
+  draw.stroke_width(1)
+
+  rmagick_draw_top_line(draw, scaled_svg.stretch_x.x, scaled_svg.stretch_x.width)
+  rmagick_draw_left_line(draw, scaled_svg.stretch_y.y, scaled_svg.stretch_y.height)
+  rmagick_draw_bottom_line(pic, draw, scaled_svg.padding_x.x, scaled_svg.padding_x.width)
+  rmagick_draw_right_line(pic, draw, scaled_svg.padding_y.y, scaled_svg.padding_y.height)
+
+  draw.draw(pic)
+
+  pic.write(input)
+end
+
+def rmagick_draw_bottom_line(picture, draw, x, length)
+  draw.line(x, picture.rows - 1, x + length, picture.rows - 1)
+end
+
+def rmagick_draw_top_line(draw, x, length)
+  draw.line(x, 0, x + length, 0)
+end
+
+def rmagick_draw_right_line(picture, draw, y, length)
+  draw.line(picture.columns - 1, y, picture.columns - 1, y + length)
+end
+
+def rmagick_draw_left_line(draw, y, length)
+  draw.line(0, y, 0, y + length)
+end
+
+# ------------------------------------------------------------------------------
+# Main
+# ------------------------------------------------------------------------------
+
+if __FILE__ == $PROGRAM_NAME then
+  path_input = ARGV[0]
+  path_output = ARGV[1]
+  exported_width = ARGV[2]
+  exported_height = ARGV[3]
+
+  if path_input == nil or path_output == nil or exported_width == nil or exported_height == nil then
+    puts "Usage: #{$PROGRAM_NAME} <input svg> <output svg> <exported width> <exported height>"
+
+    exit 1
+  end
+
+  scaled_svg = create_scaled_svg(path_input, exported_width.to_i(), exported_height.to_i())
+
+  inkscape_export_svg(scaled_svg, path_input, path_output)
+  rmagick_extend_border(path_output)
+  rmagick_add_guides(scaled_svg, path_output)
+end
+
---
+++ scalable-nine-patch/test/test_scalable-nine-patch.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/ruby -w
+#
+# test_scalable-nine-patch.rb - Test scalable-nine-patch.rb
+# Copyright (C) 2011  Lorenzo Villani
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require "rubygems"
+require "test/unit"
+
+require "scalable-nine-patch"
+
+# ------------------------------------------------------------------------------
+# Classes
+# ------------------------------------------------------------------------------
+
+class TestNinePatch < Test::Unit::TestCase
+  def setup()
+    @svg = Svg.new("test.svg")
+  end
+
+  def test_deconstruct_path_attributes()
+    # padding-x
+    result = @svg.deconstruct_path_attributes("m 188.13559,895.25424 440.67797,0")
+
+    assert_equal(188, result.x)
+    assert_equal(895, result.y)
+    assert_equal(440, result.width)
+    assert_equal(0, result.height)
+
+    # padding-y
+    result = @svg.deconstruct_path_attributes("m 900.15139,130.10169 0,579.66102")
+
+    assert_equal(900, result.x)
+    assert_equal(130, result.y)
+    assert_equal(0, result.width)
+    assert_equal(579, result.height)
+  end
+
+  def test_canvas_size()
+    assert_equal(864, @svg.width)
+    assert_equal(864, @svg.height)
+  end
+
+  def test_padding_x()
+    padding_x = @svg.padding_x
+
+    assert_equal(188, padding_x.x)
+    assert_equal(895, padding_x.y)
+    assert_equal(440, padding_x.width)
+    assert_equal(0, padding_x.height)
+  end
+
+  def test_padding_y()
+    padding_y = @svg.padding_y
+
+    assert_equal(900, padding_y.x)
+    assert_equal(130, padding_y.y)
+    assert_equal(0, padding_y.width)
+    assert_equal(579, padding_y.height)
+  end
+
+  def test_scale_half()
+    scaled = ScaledSvg.new(@svg, 432, 432)
+
+    assert_equal(94, scaled.padding_x().x)
+    assert_equal(220, scaled.padding_x().width)
+
+    assert_equal(0.5, scaled.scale_ratio_x())
+    assert_equal(0.5, scaled.scale_ratio_y())
+
+    assert_equal(432, scaled.height())
+    assert_equal(432, scaled.width())
+  end
+end
